6aed31e98beafa8b199b112b65c4a7d3c92b4e64
yagodin_artem <art-yagodin@yandex.ru>
2021-08-24 14:25:26 +0300

    AOP, deserialising, validation, annotation (fix)

--- src/main/java/ru/geekbrains/spring/Application.java
+++ src/main/java/ru/geekbrains/spring/Application.java
@@ -9,12 +9,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableConfigurationProperties
 public class Application {
 
-    /**
-     * 1. Добавить поле Производитель - Реализовать свой валидатор который проверяет компанию в каком то списке (через аннотацию @Company)
-     * 2. Product code = Название продукта - Произовдитель (яблоко - Григорьевы сады) десериализовалось в 2 отдельных поля name и company
-     * 3. Реализовать REST-controller , который предотсавляет возможности урпаавления товаром (сортировка, фильтры, удаление, добавление, обновление)
-     */
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
